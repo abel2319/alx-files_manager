3cf2e7216841c838555677727f398087a2fd34aa
abel2319 <abelbakary@gmail.com>
2023-12-08 04:22:04 +0100

    feat(task0):task 0 ✅

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log('Redis client not connected to the server:', err.toString());
+    });
+
+    this.client.on('connect', () => {});
+  }
+
+  isAlive() {
+    return this.client.connnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    return promisify(this.client.set).bind(this.client)(key, value, duration);
+  }
+
+  async del(key) {
+    return promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
